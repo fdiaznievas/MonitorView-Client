180e8dfff66749cda2804ea72d183ed2aa94d1f5
Diaz Nievas Carlos Fabricio <u901721@bancor.com.ar>
2023-04-12 15:42:07 -0300

    trabajo y modificaciones sobre el modal de tabla de novedades

--- src/app/component/tabla-novedades/tabla-modal/tabla-modal.component.ts
+++ src/app/component/tabla-novedades/tabla-modal/tabla-modal.component.ts
@@ -7,16 +7,26 @@ import { Component, Input } from '@angular/core';
 })
 export class TablaModalComponent {
 
-  infoAMostrar: any = {"id": "vacio", "active": "vacio", "cidSitio": "vacio", "sitio": "vacio", "descripcion": "vacio", "fecha": "vacio" };
+  infoAMostrar: any = {"id": "vacio", "fechaCreacion": "1/1/1990", "active": "vacio", "cidSitio": "vacio", "sitio": "vacio", "descripcion": "vacio", "fecha": "vacio" };
   @Input() selectedRow: any;
   constructor () {}
 
   MostrarSelected ():void {
     this.infoAMostrar = this.selectedRow;
+    if (this.infoAMostrar.id == "vacio") {
+    }
+    else {
+      
+    }
     console.log(this.selectedRow)  
   }
 
   LimpiarSelected ():void {
-    this.infoAMostrar = {"id": "vacio", "active": "vacio", "cidSitio": "vacio", "sitio": "vacio", "descripcion": "vacio", "fecha": "vacio" };
+    this.infoAMostrar = {"id": "vacio", "fechaCreacion": "1/1/1990", "active": "vacio", "cidSitio": "vacio", "sitio": "vacio", "descripcion": "vacio", "fecha": "vacio" };
+  }
+
+  MostrarFechaConsola ():void {
+    let prueba :any = document.getElementById("validationCustom05")
+    console.log( prueba.value)
   }
 }
